6180c2e60bb023e608d4e10c682f9e3acecb14be
meancl <sbe03253@naver.com>
2023-03-17 18:28:59 +0900

    기능별 폴더 정리

---
+++ mclLib/modelpostfix.py
@@ -0,0 +1,12 @@
+project_absolute_path = r'C:/Users/MJ/source/repos/MJTradier_AI/임시저장소'
+
+just_h5 = '/h5/'
+just_onnx = '/onnx/'
+
+h5_path = project_absolute_path + just_h5 
+onnx_path = project_absolute_path + just_onnx
+tmp_model_path = project_absolute_path + '/model_tmp'
+
+
+h5_ = '.h5'
+onnx_ = '.onnx'
--- mylib/modelpostfix.py
+++
@@ -1,8 +0,0 @@
-project_absolute_path = r'C:/Users/MJ/source/repos/MJTradier_AI'
-
-h5_path = project_absolute_path + '/h5/'  
-onnx_path = project_absolute_path + '/onnx/'
-tmp_model_path = project_absolute_path + '/empty/model_tmp/'
-
-h5_ = '.h5'
-onnx_ = '.onnx'
